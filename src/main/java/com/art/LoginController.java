9c51483aa73e86ecfcacd6c3f748a74f657e3844
linwei0045 <linwei0045@zentech-inc.com>
2018-08-28 16:04:06 +0800

    secondcomit

---
+++ src/main/java/com/art/LoginController.java
@@ -0,0 +1,12 @@
+/**
+ * Zentech-Inc
+ * Copyright (C) 2018 All Rights Reserved.
+ */
+package com.art;
+
+/**
+ * @author lw0045
+ * @version $Id LoginController.java, v 0.1 2018-08-28 11:17 lw0045 Exp $$
+ */
+public class LoginController {
+}
